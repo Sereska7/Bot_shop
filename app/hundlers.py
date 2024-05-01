886db48901f8bcca679ee5d46cb3e05df76518d7
Sereska7 <sergo_ryzakov@mail.ru>
2024-05-01 11:35:51 +0300

    app fsm_machin

--- app/hundlers.py
+++ app/hundlers.py
@@ -1,9 +1,13 @@
 from aiogram import F, Router
 from aiogram.types import Message, CallbackQuery
 from aiogram.filters import CommandStart
+from aiogram.fsm.state import State, StatesGroup
+from aiogram.fsm.context import FSMContext
+
 
 import app.keyboards as kb
 import data_base.requests as rq
+import app.fsm_machine as fs
 
 router = Router()
 
@@ -15,13 +19,15 @@ async def start_mess(message: Message):
 
 
 @router.message(F.text == "Каталог")
-async def catalog(message: Message):
+async def catalog(message: Message, state: FSMContext):
+    await state.set_state(fs.ItemBack.id_item)
     await message.answer("Выберите категорю товара",
                          reply_markup=await kb.categories())
 
 
 @router.callback_query(F.data.startswith('category_'))
-async def categories(callback: CallbackQuery):
+async def categories(callback: CallbackQuery, state: FSMContext):
+    await state.update_data(id_item=int(callback.data.split('_')[1]))
     await callback.answer("Вы выбрали категорию")
     await callback.message.answer('Выберите товар',
                                   reply_markup=await kb.items(int(callback.data.split('_')[1])))
@@ -50,5 +56,8 @@ async def backward_to(callback: CallbackQuery):
 
 
 @router.callback_query(F.data.startswith("ago_item"))
-async def ago_to_item(callback: CallbackQuery):
-    pass
+async def ago_to_item(callback: CallbackQuery, state: FSMContext):
+    data = await state.get_data()
+    await callback.answer("Вы выбрали категорию")
+    await callback.message.answer('Выберите товар',
+                                  reply_markup=await kb.items(data['id_item']))
